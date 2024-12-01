4517021babcd144b2b9b021b5bff139a56aae3bc
Mangesh Bide <mangeshbide123@gmail.com>
2024-12-01 16:51:20 +0530

    basic backend setup with typescript and nodemon for watching project

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,1 +1,17 @@
-// hello server code here
+import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello, TypeScript!');
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
