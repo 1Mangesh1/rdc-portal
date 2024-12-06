bb04f810dfa56648fc624d828f9afbcc87f8f5e8
Mangesh Bide <mangeshbide123@gmail.com>
2024-12-06 17:04:25 +0530

    naming conventions added

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,16 +1,13 @@
 import express, { Request, Response } from 'express';
 import dotenv from 'dotenv';
-import { getUsers, createUser } from './controllers/userController';
-dotenv.config();
 import {userRouter} from "./routes/userRoutes"
 import { videoRouter } from './routes/videoRouter';
+dotenv.config();
+
 const app = express();
 const port = process.env.PORT || 3000;
 
-
-
 app.use(express.json());
-
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello, TypeScript!');
@@ -18,8 +15,6 @@ app.get('/', (req: Request, res: Response) => {
 
 app.use("/users",userRouter)
 app.use("/videos",videoRouter)
-
-
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
--- backend/src/config/Connection.ts
+++ backend/src/config/Connection.ts
@@ -1,17 +1,17 @@
 const { Sequelize, DataTypes } = require('sequelize');
-const Pool = require('pg').Pool
+// const Pool = require('pg').Pool
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 
-export const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT
-})
+// export const pool = new Pool({
+//   user: process.env.DB_USER,
+//   host: process.env.DB_HOST,
+//   database: process.env.DB_NAME,
+//   password: process.env.DB_PASSWORD,
+//   port: process.env.DB_PORT
+// })
 
 export const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
