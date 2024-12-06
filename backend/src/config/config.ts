fb2a4e16473b14f4084155537ef31f2312ece12d
Mangesh Bide <mangeshbide123@gmail.com>
2024-12-06 11:30:19 +0530

    setup config.ts

---
+++ backend/src/config/config.ts
@@ -0,0 +1,32 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+
+module.exports = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT,
+  },
+  // test: {
+  //   username: process.env.DB_USER,
+  //   password: process.env.DB_PASSWORD,
+  //   database: process.env.DB_NAME_TEST,
+  //   host: process.env.DB_HOST,
+  //   port: parseInt(process.env.DB_PORT, 10),
+  //   dialect: process.env.DB_DIALECT,
+  // },
+  // production: {
+  //   username: process.env.DB_USER,
+  //   password: process.env.DB_PASSWORD,
+  //   database: process.env.DB_NAME_PROD,
+  //   host: process.env.DB_HOST,
+  //   port: parseInt(process.env.DB_PORT, 10),
+  //   dialect: process.env.DB_DIALECT',
+  // },
+};
+  
